30de96330ef43879da3f02d45b8c9033ef740d32
danhooper <dan.c.hooper@gmail.com>
2014-04-26 12:47:02 -0400

    Added a static file

--- src/home_page/static/js/rss_reader.js
+++ src/home_page/static/js/rss_reader.js
@@ -6,7 +6,7 @@ var RssReader = {
     feedExpand: function(event) {
         event.preventDefault();
         target_a = event.target;
-        if ($(event.target).is('i')) {
+        if ($(event.target).is('span')) {
             target_a = $(event.target).parent();
         }
         entry_wrapper = $(target_a).nextAll('.entry_wrapper').slice(0, 1);
@@ -15,15 +15,15 @@ var RssReader = {
             $(entry_wrapper).css('display', 'block');
             $(entry_wrapper).addClass('visible');
             $(entry_wrapper).removeClass('hidden');
-            $(target_a).children('.icon-plus').addClass('icon-minus');
-            $(target_a).children('.icon-plus').removeClass('icon-plus');
+            $(target_a).children('.glyphicon-plus').addClass('glyphicon-minus');
+            $(target_a).children('.glyphicon-plus').removeClass('glyphicon-plus');
         } else {
             console.log('Hiding');
             $(entry_wrapper).css('display', 'none');
             $(entry_wrapper).addClass('hidden');
             $(entry_wrapper).removeClass('visible');
-            $(target_a).children('.icon-minus').addClass('icon-plus');
-            $(target_a).children('.icon-minus').removeClass('icon-minus');
+            $(target_a).children('.glyphicon-minus').addClass('glyphicon-plus');
+            $(target_a).children('.glyphicon-minus').removeClass('glyphicon-minus');
         }
     },
     refreshFeed: function(event) {
