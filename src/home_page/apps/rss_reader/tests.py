2bb5bd119a377ca131d47f37b374639da5eac71c
danhooper <dan.c.hooper@gmail.com>
2014-04-28 21:16:38 -0400

    Fixing unit tests

--- src/home_page/apps/rss_reader/tests.py
+++ src/home_page/apps/rss_reader/tests.py
@@ -1,4 +1,5 @@
 import unittest
+from django.contrib.auth.models import User
 from django.test import Client
 import models
 
@@ -6,9 +7,14 @@ class TestRSSReader(unittest.TestCase):
 
     def setUp(self):
         self.client = Client()
+        self.user = User.objects.create_user(
+            username='test', email='test@example.com', password='password')
+        self.user.save()
+        self.client.login(username='test', password='password')
+
 
     def test_feeds(self):
-        response = self.client.get('/rss_reader/')
+        response = self.client.get('/home_page/rss_reader/')
         self.assertEqual(response.status_code, 200)
 
     #def test_feed(self):
