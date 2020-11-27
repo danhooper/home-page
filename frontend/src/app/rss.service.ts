4db1df136c50b3e12496784a13c7e8689bd0a49d
Dan Hooper <dan.c.hooper@gmail.com>
2020-11-27 09:31:26 -0500

    Improved frontend logic for fetching articles

--- frontend/src/app/rss.service.ts
+++ frontend/src/app/rss.service.ts
@@ -2,8 +2,8 @@ import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {RssArticle, IRssArticle} from './model/RssArticle';
 import {RssFeed, IRssFeed} from './model/RssFeed';
-import {Observable, ReplaySubject} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {timer, Observable} from 'rxjs';
+import {map, switchMap} from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root',
@@ -20,33 +20,9 @@ export class RssService {
     }
 
     getArticles(feed: RssFeed): Observable<RssArticle[]> {
-        const articleSubject = new ReplaySubject<RssArticle[]>(1);
-        this.fetchArticles(feed, articleSubject);
-        return articleSubject.asObservable();
-    }
-
-    private fetchArticles(
-        feed: RssFeed,
-        articleSubject: ReplaySubject<RssArticle[]>,
-    ) {
-        return this.http
-            .get<IRssArticle[]>(`backend/rss/${feed.id}/article`)
-            .pipe(
-                map((result) => {
-                    return result.map((article) => new RssArticle(article));
-                }),
-            )
-            .subscribe((articles) => {
-                if (articleSubject.observers.length > 0) {
-                    articleSubject.next(articles);
-                    feed.lastUpdatedAt = new Date();
-                    setTimeout(
-                        () => this.fetchArticles(feed, articleSubject),
-                        600000,
-                    );
-                } else {
-                    articleSubject.complete();
-                }
-            });
+        return timer(0, 600000).pipe(
+            switchMap(() => this.http.get<IRssArticle[] | null>(`backend/rss/${feed.id}/article`)),
+            map(result => (result || []).map((article) => new RssArticle(article))),
+        );
     }
 }
