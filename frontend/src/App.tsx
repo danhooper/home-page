bbb13b79842d87e3e4084e10ce28e939a7f3287d
Dan Hooper <dan.c.hooper@gmail.com>
2020-12-20 14:02:07 -0500

    tmp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,10 +6,13 @@ import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import ReactHtmlParser from 'react-html-parser';
+import { DomElement } from "htmlparser2";
+import ReactHtmlParser, {Transform} from 'react-html-parser';
 import './App.css';
 import {IRssArticle, RssArticle} from './models/RssArticle';
 import {IRssFeed, RssFeed} from './models/RssFeed';
+
+const TEN_MINUTES_IN_MS = 10 * 60 * 1000;
 
 const theme = createMuiTheme({
   palette: {
@@ -31,16 +34,19 @@ const useStyles = makeStyles(() => ({
   root: {
     flexGrow: 1,
     backgroundColor: theme.palette.primary.main,
+    minHeight: '100vh',
+    paddingBlock: '30px',
   },
   feedDetail: {
     border: `1px solid ${theme.palette.primary.dark}`,
     borderRadius: '3px',
-    margin: '15px 7px 0 7px',
+    margin: '15px 10px 0 10px',
   },
   feedHeading: {
     fontSize: theme.typography.pxToRem(20),
     fontWeight: theme.typography.fontWeightBold,
     textAlign: 'center',
+    padding: '5px',
     marginBottom: '5px',
   },
   feedLink: {
@@ -51,15 +57,27 @@ const useStyles = makeStyles(() => ({
     fontWeight: theme.typography.fontWeightRegular,
   },
   articleDetail: {
+    '& a': {
+      color: theme.palette.secondary.main,
+    },
     '& img': {
       maxWidth: '100%',
       objectFit: 'contain',
+    },
+    '& .feedflare': {
+      height: 0,
+      display: 'none',
+    },
+  },
+  accordion: {
+    '&.Mui-expanded': {
+      backgroundColor: theme.palette.primary.dark,
     }
   },
   accordionSummary: {
     '&:hover': {
       backgroundColor: theme.palette.secondary.dark,
-    }
+    },
   }
 }));
 
@@ -84,10 +102,16 @@ function RssFeedList(props: {feeds: RssFeed[]}) {
   const feeds = props.feeds.map((feed) => (<Grid item md={4} sm={12} key={feed.id}><RssFeedDetail feed={feed} /></Grid>));
 
   return (
-    <Grid container spacing={1}>
+    <Grid container spacing={0}>
       {feeds}
     </Grid>
   );
+}
+
+async function fetchArticles(feedId: number): Promise<RssArticle[]> {
+      const response = await fetch(`/rss/${feedId}/article`);
+      const articles: IRssArticle[] = await response.json();
+  return articles.slice(0, 10).map(art => new RssArticle(art));
 }
 
 function RssFeedDetail(props: {feed: RssFeed}) {
@@ -95,11 +119,24 @@ function RssFeedDetail(props: {feed: RssFeed}) {
   const [articles, setArticles] = useState<RssArticle[]>([]);
   const classes = useStyles();
 
+  const onChange = (event: any, expanded: boolean) => {console.log(event, expanded)}
+
   useEffect(() => {
-    fetch(`/rss/${feed.id}/article`)
-    .then(response => response.json())
-    .then((articles: IRssArticle[]) => setArticles(articles.slice(0, 10).map(art => new RssArticle(art))));
+  const fetcher = async () =>  {
+      const articles = await fetchArticles(feed.id);
+      setArticles(articles);
+  };
+    fetcher();
+    setInterval(fetcher, TEN_MINUTES_IN_MS);
   }, [feed.id]);
+
+const transform: Transform = (node: DomElement) => {
+  // if (node.type === 'tag' && node.name = 'a') {
+  //   node.a
+  // }
+  // console.log(node);
+  return;
+}
 
   return (
     <div className={classes.feedDetail}>
@@ -109,7 +146,7 @@ function RssFeedDetail(props: {feed: RssFeed}) {
         </a>
         </div>
       {articles.map(article => (
-        <Accordion TransitionProps={{ unmountOnExit: true }} key={article.title + article.url}>
+        <Accordion TransitionProps={{ unmountOnExit: true }} key={article.title + article.url} onChange={onChange} className={classes.accordion}>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon color="secondary" />}
             className={classes.accordionSummary}
@@ -118,7 +155,7 @@ function RssFeedDetail(props: {feed: RssFeed}) {
           </AccordionSummary>
           <AccordionDetails className={classes.articleDetail}>
             <Typography>
-              {ReactHtmlParser(article.longDescription)}
+              {ReactHtmlParser(article.longDescription, {transform})}
             </Typography>
           </AccordionDetails>
         </Accordion>
